d1c54c5c0dac7de1a00a9b7874960045170055ac
Sivaranjani Rajasekaran <sivaranjani.r@syncfusion.com>
2026-05-11 16:59:28 +0530

    Documentation(1023563): Update Package

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [NavMenuComponent, RouterModule]
 })
 export class AppComponent {
   title = 'app';
